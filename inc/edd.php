6bae5b1dc773739fb9b24430684bb60cf15dc68d
Bernard Meisler <bmeisler@gmail.com>
2015-11-06 20:32:19 -0800

    set Thangs back to Downloads

--- inc/edd.php
+++ inc/edd.php
@@ -1,19 +1,19 @@
 <?php
 function set_download_labels($labels) {
 	$labels = array(
-		'name' => _x('Thangs', 'post type general name', 'sensitive-skin-bootstrap'),
-		'singular_name' => _x('Thang', 'post type singular name', 'sensitive-skin-bootstrap'),
+		'name' => _x('Downloads', 'post type general name', 'sensitive-skin-bootstrap'),
+		'singular_name' => _x('Download', 'post type singular name', 'sensitive-skin-bootstrap'),
 		'add_new' => __('Add New', 'sensitive-skin-bootstrap'),
-		'add_new_item' => __('Add New Thang', 'sensitive-skin-bootstrap'),
-		'edit_item' => __('Edit Thang', 'sensitive-skin-bootstrap'),
-		'new_item' => __('New Thang', 'sensitive-skin-bootstrap'),
-		'all_items' => __('All Thangs', 'sensitive-skin-bootstrap'),
-		'view_item' => __('View Thang', 'sensitive-skin-bootstrap'),
-		'search_items' => __('Search Thangs', 'sensitive-skin-bootstrap'),
-		'not_found' =>  __('No Thangs found', 'sensitive-skin-bootstrap'),
-		'not_found_in_trash' => __('No Thangs found in Trash', 'sensitive-skin-bootstrap'), 
+		'add_new_item' => __('Add New Download', 'sensitive-skin-bootstrap'),
+		'edit_item' => __('Edit Download', 'sensitive-skin-bootstrap'),
+		'new_item' => __('New Download', 'sensitive-skin-bootstrap'),
+		'all_items' => __('All Downloads', 'sensitive-skin-bootstrap'),
+		'view_item' => __('View Download', 'sensitive-skin-bootstrap'),
+		'search_items' => __('Search Downloads', 'sensitive-skin-bootstrap'),
+		'not_found' =>  __('No Downloads found', 'sensitive-skin-bootstrap'),
+		'not_found_in_trash' => __('No Downloads found in Trash', 'sensitive-skin-bootstrap'), 
 		'parent_item_colon' => '',
-		'menu_name' => __('Thangs', 'sensitive-skin-bootstrap')
+		'menu_name' => __('Downloads', 'sensitive-skin-bootstrap')
 	);
 	return $labels;
 }
