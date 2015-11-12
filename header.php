6e0bc2f648c887f2db84fa556f8304af19cd7f25
Bernard Meisler <bmeisler@gmail.com>
2015-11-11 21:55:17 -0800

    clean up on navbar; removed h1 for site name

--- header.php
+++ header.php
@@ -25,89 +25,68 @@
 
 <body <?php body_class(); ?>>
 	<div id="container">
-<div id="page" class="hfeed site">
+		<div id="page" class="hfeed site">
 <!-- 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'sensitive-skin-bootstrap' ); ?></a>
- -->
-<div class="hidden">
-	 <h1>Sensitive Skin Magazine</h1>
-                    </div>
-<!--  	<div id="headerimage">
-                    	
-                        <?php if (is_home()) { ?> 
-                        	<h1>Sensitive Skin Magazine</h1>
-                        <?php } else { ?>
-                        	<h3>Sensitive Skin Magazine</h3>
-                        <?php } ?>
-                        <figure id="figure-logo">
-                        <a href="<?php bloginfo('url'); ?>" >
-                        	<img src="http://www.sensitiveskinmagazine.com/wp-content/images/SSMLogo.svg" />
-                        </a>
-                        </figure>
-                    </div> -->
+ -->	<header>
+			<nav role="navigation" class="navbar navbar-default navbar-fixed-top">
+				<div class="container-fluid navbar-static-top" ><!-- class="navbar navbar-static-top navbar-inverse" -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
 
-		<nav role="navigation" class="navbar navbar-default navbar-fixed-top">
-		<div class="container-fluid navbar-static-top" ><!-- class="navbar navbar-static-top navbar-inverse" -->
-<!-- 			<div class="container">
- -->				<!-- .navbar-toggle is used as the toggle for collapsed navbar content -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="<?php bloginfo('url'); ?>">
+							<img src="http://www.sensitiveskinmagazine.com/wp-content/images/SSMLogo.svg" width="200" height="auto" />
+						</a>
 
-					</button>
-					<a class="navbar-brand" href="<?php bloginfo('url'); ?>">
-						<img src="http://www.sensitiveskinmagazine.com/wp-content/images/SSMLogo.svg" width="200" height="auto" />
-					</a>
-
-					<!-- <div id="awesome-social-buttons">
+							<!-- <div id="awesome-social-buttons">
+								
+							
+								<a class="btn btn-social-icon btn-sm " href="http://www.twitter.com/sensitivemag">
+			    					<span class="fa fa-twitter"></span>
+			  					</a>
+			  					<a class="btn btn-social-icon btn-sm" href="http://www.facebook.com/sensitiveskin">
+								    <span class="fa fa-facebook"></span>
+								</a>
+								<a class="btn btn-social-icon btn-sm btn-google-plus" <a href="//plus.google.com/100022193362098500932?prsrc=3">
+								    <span class="fa fa-google-plus"></span>
+								</a>
+							
+		  					</div> -->
+		<!-- 					<a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
+		 -->					<!-- <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ) ?>" rel="homepage"><?php bloginfo( 'name' ) ?></a> -->
+					</div><!-- navbar-header -->
 						
-					
-						<a class="btn btn-social-icon btn-sm " href="http://www.twitter.com/sensitivemag">
-	    					<span class="fa fa-twitter"></span>
-	  					</a>
-	  					<a class="btn btn-social-icon btn-sm" href="http://www.facebook.com/sensitiveskin">
-						    <span class="fa fa-facebook"></span>
-						</a>
-						<a class="btn btn-social-icon btn-sm btn-google-plus" <a href="//plus.google.com/100022193362098500932?prsrc=3">
-						    <span class="fa fa-google-plus"></span>
-						</a>
-					
-  					</div> -->
-<!-- 					<a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a>
- -->					<!-- <a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php bloginfo( 'name' ) ?>" rel="homepage"><?php bloginfo( 'name' ) ?></a> -->
-				</div>
-				
-				<div class="navbar-collapse collapse navbar-responsive-collapse">
+					<div class="navbar-collapse collapse navbar-responsive-collapse">
 
-					<form class="navbar-form navbar-left" role="search">
-			        <div class="form-group">
-			          <input type="text" class="form-control search-query" name="s" placeholder="Search">
-			        </div>
-			        <button type="submit" class="btn btn-default" id="searchsubmit" value="Search">
-			        <span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
-			      </form>
+						<form class="navbar-form navbar-left" role="search">
+				        	<div class="form-group">
+				          		<input type="text" class="form-control search-query" name="s" placeholder="Search">
+				        	</div>
+				        	<button type="submit" class="btn btn-default" id="searchsubmit" value="Search">
+				        	<span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+				   	   	</form>
 
-					<?php
+						<?php
 
-					$args = array(
-						'theme_location' => 'primary',
-						'depth'      => 2,
-						'container'  => false,
-						'menu_class'     => 'nav navbar-nav navbar-right',
-						'walker'     => new Bootstrap_Walker_Nav_Menu()
-						);
+						$args = array(
+							'theme_location' => 'primary',
+							'depth'      => 2,
+							'container'  => false,
+							'menu_class'     => 'nav navbar-nav navbar-right',
+							'walker'     => new Bootstrap_Walker_Nav_Menu()
+							);
 
-					if (has_nav_menu('primary')) {
-						wp_nav_menu($args);
-					}
+						if (has_nav_menu('primary')) {
+							wp_nav_menu($args);
+						}
 
-					?>
-				</div>
-
-			<!-- </div> -->
-		</div>           
-	</nav>
-	</header><!-- #masthead -->
+						?>
+					</div><!-- navbar-collapse -->
+				</div><!-- container-fluid -->           
+			</nav>	
+		</header><!-- #masthead -->
 
 	<div id="content" class="site-content">
