8f385aaa2b83515497be80b241c5d8b0933f253b
Bernard Meisler <bmeisler@gmail.com>
2015-11-12 13:59:36 -0800

    enqued js for Nancy cut=up

--- functions.php
+++ functions.php
@@ -244,6 +244,9 @@ function sensitive_skin_bootstrap_scripts() {
 
 	wp_enqueue_script( 'sensitive-skin-bootstrap-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
 
+	wp_enqueue_script('preload', "https://code.createjs.com/createjs-2015.05.21.min.js", array(), '0.6.0', true);
+    wp_enqueue_script( 'random-image',  '/wp-content/js/random-image-dnd.js', array('preload', 'underscore'), '1.0.0', true );
+
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
