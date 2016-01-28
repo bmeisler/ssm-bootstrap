ffb9f81c89f1770d901639c6517e451977d9a831
Bernard Meisler <bmeisler@gmail.com>
2016-01-28 13:01:02 -0800

    HTML validation. Using an include file now for the mailchimp signup form on top and bottom. Changed id to class so no duplicates.

--- footer.php
+++ footer.php
@@ -96,46 +96,8 @@
        <!-- Begin MailChimp Signup Form -->
         <div id="bottom-signup">
 
-            <div id="mc_embed_floating_signup">
-                      <form action="//sensitiveskinmagazine.us2.list-manage.com/subscribe/post?u=df2b62f883c43ba3749ff7368&amp;id=c53aaba024" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-
-                  <input type="hidden" name="u" value="5e0434478e96f6841daff1690">
-                  <input type="hidden" name="id" value="5854671be8"> 
-
-                        <div id="mc_embed_signup_scroll">
-                          <div class="mc-field-group">
-                            <input type="email" value="" name="EMAIL" placeholder="Email Address (required)" onfocus="this.placeholder=''" onblur="this.placeholder='Email Address'" class="required form-control input-lg" id="mce-EMAIL">
-                          </div>
-                          <div class="row form-group">
-                            <div class="col-xs-6 input-lg-left">
-                                <div class="mc-field-group">
-                              
-                                  <input type="text" value="" name="FNAME" placeholder="First Name" onfocus="this.placeholder=''" onblur="this.placeholder='First Name'" class="form-control input-lg" id="mce-FNAME">
-                                </div><!-- mc-field-group -->
-                            </div><!-- col-xs-6 -->
-                            <div class="col-xs-6 input-lg-right">
-                                <div class="mc-field-group">
-                                  <input type="text" value="" name="LNAME" placeholder="Last Name" onfocus="this.placeholder=''" onblur="this.placeholder='Last Name'" class="form-control input-lg" id="mce-LNAME">
-                                </div><!-- mc-field-group -->
-                            </div><!-- col-xs-6 -->
-                          </div><!-- row form-group -->
-                          <div id="mce-responses" class="clear">
-                            <div class="response" id="mce-error-response" style="display:none">
-                            </div>
-                            <div class="response" id="mce-success-response" style="display:none">
-                            </div>
-                          </div><!-- mce-responses -->
-                          
-                          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-                          <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                            <input type="text" name="b_df2b62f883c43ba3749ff7368_c53aaba024" tabindex="-1" value="">
-                          </div><!-- hidden div -->
-                          <div class="clear">
-                            <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="btn btn-primary">
-                          </div><!-- clear -->
-                        </div><!--End mc_embed_signup_scroll-->
-                      </form>
-                    </div> 
+            <?php include('inc/mailchimp-signup-form.php');?>
+            
         </div>
 		</div><!-- end 3rd column -->
       </div><!-- row -->
---
+++ inc/mailchimp-signup-form.php
@@ -0,0 +1,40 @@
+<div class="mc_embed_floating_signup">
+	<form action="//sensitiveskinmagazine.us2.list-manage.com/subscribe/post?u=df2b62f883c43ba3749ff7368&amp;id=c53aaba024" method="post"  name="mc-embedded-subscribe-form" class="mc-embedded-subscribe-form validate" target="_blank" novalidate>
+
+		<input type="hidden" name="u" value="5e0434478e96f6841daff1690">
+		<input type="hidden" name="id" value="5854671be8"> 
+
+		<div class="mc_embed_signup_scroll">
+	      <div class="mc-field-group">
+	        <input type="email" value="" name="EMAIL" placeholder="Email Address (required)" onfocus="this.placeholder=''" onblur="this.placeholder='Email Address'" class="required form-control input-lg mce-EMAIL">
+	      </div>
+	  	<div class="row form-group">
+	    	<div class="col-xs-6 input-lg-left">
+	      		<div class="mc-field-group">
+	        
+	        		<input type="text" value="" name="FNAME" placeholder="First Name" onfocus="this.placeholder=''" onblur="this.placeholder='First Name'" class="form-control input-lg mce-FNAME">
+	      		</div><!-- mc-field-group -->
+	    	</div><!-- col-xs-6 -->
+	    	<div class="col-xs-6 input-lg-right">
+	      		<div class="mc-field-group">
+	        		<input type="text" value="" name="LNAME" placeholder="Last Name" onfocus="this.placeholder=''" onblur="this.placeholder='Last Name'" class="form-control input-lg mce-LNAME">
+	      		</div><!-- mc-field-group -->
+	    	</div><!-- col-xs-6 -->
+	    </div><!-- row form-group -->
+	    <div class=" mce-responses clear">
+	      <div class="response mce-error-response" style="display:none">
+	      </div>
+	      <div class="response mce-success-response" style="display:none">
+	      </div>
+	    </div><!-- mce-responses -->
+	    
+	    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+	    <div style="position: absolute; left: -5000px;" aria-hidden="true">
+	      <input type="text" name="b_df2b62f883c43ba3749ff7368_c53aaba024" tabindex="-1" value="">
+	    </div><!-- hidden div -->
+	    <div class="clear">
+	      <input type="submit" value="Subscribe" name="subscribe" class="mc-embedded-subscribe btn btn-primary">
+	    </div><!-- clear -->
+	  </div><!--End mc_embed_signup_scroll-->
+	</form>
+</div> 
