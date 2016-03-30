a316e62f9716ff2ca19f64c954f489279a120631
Bernard Meisler <bmeisler@gmail.com>
2016-03-29 19:14:03 -0700

    Removed animated gif from sidebar replaced with static image of issue 8

--- inc/sidebar-Animated.php
+++ inc/sidebar-Animated.php
@@ -7,7 +7,7 @@
 	<?php global $orig_post; //print_r($orig_post->ID);
                 //print_r($post->ID);
                 //print_r($orig_post->ID);?>
-                <a href="<?php bloginfo('wpurl'); ?>/back-issues/" target="_blank"><img src="<?php bloginfo('wpurl'); ?>/wp-content/images/AnimatedCovers.gif" alt="Sensitive Skin #12"   width="300" height="372" /></a>
+                <a href="<?php bloginfo('wpurl'); ?>/back-issues/" target="_blank"><img src="<?php bloginfo('wpurl'); ?>/wp-content/images/covers/Number_8_cover_300w.jpg" alt="Sensitive Skin #8 William S. Burroughs"   width="300" height="372" /></a>
 
                 <p class="sidebarCopyCentered"><strong><a href="<?php bloginfo('wpurl'); ?>/downloads-store/">Every issue</a> of Sensitive Skin is now available in a full-color, interactive PDF edition!</strong> Looks great on iPads or other tablets! For those who prefer print, you can get issues 8 and up at <a href="http://www.amazon.com/Sensitive-Skin-12-Bernard-Meisler/dp/099615700X/ref=sr_1_1?ie=UTF8&qid=1437611644&sr=8-1&keywords=sensitive+skin+magazine+%2312" target="_blank"><strong>Amazon</strong></a>.</p>
 
