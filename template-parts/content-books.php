8d8e0cbd54b4cceef85726c7d41f70e0ad908b59
Bernard Meisler <bmeisler@gmail.com>
2016-06-24 12:33:11 -0700

    slight tweaks to adding pdf button to individual book and list of books

--- template-parts/content-books.php
+++ template-parts/content-books.php
@@ -20,13 +20,11 @@
 				<a role="button" class="btn btn-primary offset5" href="<?php the_field('kindle_link') ?>" >Buy it on Kindle</a>
 		<?php } ?>
 
-		<?php if (get_field('pdf_link') !== ''){
-				$postID = get_field('pdf_link');
-				?>
-
-				<a class="btn btn-primary" role="button" href="<?php bloginfo('url'); ?>/checkout?edd_action=add_to_cart&download_id=<?php the_field('pdf_link') ?>; ?>"><?php edd_price($postID);?> Buy the PDF</a>
-
-		<?php } ?>
+		<?php if (get_field('pdf_link') !== ''){//enter the download ID in the pdf_link field for the book
+									$postID = get_field('pdf_link');
+									?>
+									<a class="btn btn-primary" role="button" href="<?php bloginfo('url'); ?>/checkout?edd_action=add_to_cart&download_id=<?php the_field('pdf_link') ?>; ?>"><?php edd_price($postID);?> Buy the PDF</a>
+							<?php } ?>
     	<!-- </div> -->
    
 
