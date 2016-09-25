24cf880c38ca390bab0ad155a9d30f2dbcda8e95
Bernard Meisler <bmeisler@gmail.com>
2016-09-25 12:40:07 -0700

    search results now look like archives with correct page navigation

--- search.php
+++ search.php
@@ -28,12 +28,12 @@ get_header(); ?>
 				 * If you want to overload this in a child theme then include a file
 				 * called content-search.php and that will be used instead.
 				 */
-				get_template_part( 'template-parts/content', 'search' );
+				get_template_part( 'template-parts/content-frontpage', 'search' );
 				?>
 
 			<?php endwhile; ?>
-
-			<?php the_posts_navigation(); ?>
+			<?php if (function_exists('wp_corenavi')) wp_corenavi(); ?>
+			<?php //the_posts_navigation(); ?>
 
 		<?php else : ?>
 
